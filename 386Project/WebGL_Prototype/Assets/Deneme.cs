3521a87288567826f60db560e75c49651d9022a9
Doğa Sarp Sezer <dogasarpsezer24@gmail.com>
2022-03-05 01:38:04 +0300

    Sprite commit

--- 386Project/WebGL_Prototype/Assets/Deneme.cs
+++ 386Project/WebGL_Prototype/Assets/Deneme.cs
@@ -1,22 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+using Cursor = UnityEngine.Cursor;
 
 public class Deneme : MonoBehaviour
 {
+    [SerializeField] private Transform playerPivot;
+    [SerializeField] private Transform crossairPivot;
+    [SerializeField] private Transform muzzle;
+    [SerializeField] private Transform arm;
+    [SerializeField] Vector3 planeNormal;
     private Camera mainCamera;
-    // Start is called before the first frame update
+    private Plane inputPlane;
+
     void Start()
     {
+        Cursor.visible = false;
         mainCamera = Camera.main;
+        inputPlane = new Plane(planeNormal, Vector3.zero);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        var mouseScreenPosRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (inputPlane.Raycast(mouseScreenPosRay,out float enter))
         {
-            mainCamera.backgroundColor = new Color(Random.Range(0f,1f),Random.Range(0f,1f), Random.Range(0f,1f),1);
+            Vector3 inputPos = mouseScreenPosRay.GetPoint(enter);
+
+            inputPos = Vector3.ProjectOnPlane(inputPos, inputPlane.normal);
+            crossairPivot.position = inputPos;
+            var angle =  Mathf.Rad2Deg * Mathf.Atan2(crossairPivot.position.y - muzzle.position.y, crossairPivot.position.x - muzzle.position.x);
+            arm.Rotate(Vector3.forward,angle);
         }
     }
 }
