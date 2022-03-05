84e20b56a23dfaee63af49839f49f4f8de2fc30d
Doğa Sarp Sezer <dogasarpsezer24@gmail.com>
2022-03-05 04:17:34 +0300

    Aim Fınıshed

--- 386Project/WebGL_Prototype/Assets/Deneme.cs
+++ 386Project/WebGL_Prototype/Assets/Deneme.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,7 +9,7 @@ public class Deneme : MonoBehaviour
 {
     [SerializeField] private Transform playerPivot;
     [SerializeField] private Transform crossairPivot;
-    [SerializeField] private Transform muzzle;
+    [SerializeField] private Vector2 rotationLimits;
     [SerializeField] private Transform arm;
     [SerializeField] Vector3 planeNormal;
     private Camera mainCamera;
@@ -16,7 +17,7 @@ public class Deneme : MonoBehaviour
 
     void Start()
     {
-        Cursor.visible = false;
+        //Cursor.visible = false;
         mainCamera = Camera.main;
         inputPlane = new Plane(planeNormal, Vector3.zero);
     }
@@ -28,11 +29,31 @@ public class Deneme : MonoBehaviour
         if (inputPlane.Raycast(mouseScreenPosRay,out float enter))
         {
             Vector3 inputPos = mouseScreenPosRay.GetPoint(enter);
-
             inputPos = Vector3.ProjectOnPlane(inputPos, inputPlane.normal);
             crossairPivot.position = inputPos;
-            var angle =  Mathf.Rad2Deg * Mathf.Atan2(crossairPivot.position.y - muzzle.position.y, crossairPivot.position.x - muzzle.position.x);
-            arm.Rotate(Vector3.forward,angle);
+
+            float angleInit = 0;
+            float limitX = rotationLimits.x;
+            float limitY = rotationLimits.y;
+            if (crossairPivot.localPosition.x < 0)
+            {
+                angleInit = 180;
+                playerPivot.localScale= new Vector3(-1,1,1);
+            }
+            else
+            {
+                playerPivot.localScale= new Vector3(1,1,1);
+            }
+            
+            arm.eulerAngles = new Vector3(arm.eulerAngles.x,arm.eulerAngles.y,CalculateAngelToGo(angleInit,inputPos,arm.position));
         }
+    }
+
+    public float CalculateAngelToGo(float angleInit, Vector3 pos1, Vector3 pos2)
+    {
+        var lookDir = pos1 - pos2;
+        var angle =  Mathf.Rad2Deg * Mathf.Atan2(lookDir.y,lookDir.x) * playerPivot.localScale.x;
+        angle = angleInit + angle * playerPivot.localScale.x;
+        return angle;
     }
 }
